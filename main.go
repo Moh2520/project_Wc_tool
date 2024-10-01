44282552fd2aa3d456a0d07dff535b522300e764
moha1113 <mohamedibrahimma12@gmail.com>
2024-10-01 08:03:52 -0500

    made some good progress

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	// "bytes"
 	"flag"
 	"fmt"
 	"os"
@@ -9,48 +10,106 @@ import (
 )
 
 
-	func word_count() int {
+	func word_count(data string) int {
 
 		sum := 0
-		data := "This is a sample sentence"
 		words := strings.Fields(data)
 
 		for _, word := range words {
 			fmt.Println(word)
 			sum++
 		}
-		fmt.Println(sum)
+		// fmt.Println(sum)
 		return sum
 	
 	}
 
-func main(){
-	read in file 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Hey Please enter your file")
-	filename, err := reader.ReadString('\n')
+	func char_count(data string) int {
+		sum := 0
 
-	if err != nil {
-		fmt.Println(err)
+		for _, char := range data {
+			sum++
+			fmt.Println(char)
+		}
+		return sum
 	}
 
-	// content, err := os.ReadFile(filename)
-	
+	func line_count(data string) int {
+		number_of_lines := 0
+		lines := strings.Split(data, "\n")
 
-	// data := string(content)
-
-	fmt.Println("file is" + filename)
-
-	words := flag.Bool("w", false, "counts words")
-
-	flag.Parse()
-	// * is a pointer and now we are derefencing
-	if *words {
-		word_count()
-		// count all the words 
-
-	word_count()
+		for _, line := range lines {
+			fmt.Println(line)
+			number_of_lines ++
+		}
+		return number_of_lines
 	}
 
+	func main(){
+		reader := bufio.NewReader(os.Stdin)
 
-}
+		words := flag.Bool("w", false, "counts words")
+		bytes := flag.Bool("c", false, "counts bytes")
+		lines := flag.Bool("l", false, "counts lines")
+		characters := flag.Bool("m", false, "counts characters")
+		defaultt := flag.Bool("", false, "counts characters")
+
+
+
+		flag.Parse()
+
+		fmt.Println("Hey Please enter your file")
+		filename, err := reader.ReadString('\n')
+		// fmt.Println(filename)
+
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		filename = strings.TrimSpace(filename)
+
+		content, err := os.ReadFile(filename)
+		// fmt.Println(content)
+
+
+		data := string(content)
+
+		// fmt.Println(data)
+
+		// fmt.Println("file is " + filename)
+
+
+		// * is a pointer and now we are derefencing
+		if *defaultt == true {
+			fmt.Println(word_count(data), len(content), line_count(data), char_count(data))
+		}
+		if *words == true {
+			// fmt.Println("we made it to word count")
+			word_count := word_count(data)
+			fmt.Println(word_count)
+			// count all the words 
+		} 
+		if *bytes == true {
+
+			byte_sum := len(content)
+			fmt.Println(byte_sum)
+		
+		} 
+		if *lines == true {
+			line_count := line_count(data)
+			fmt.Println(line_count)
+		} 
+		
+		if *characters == true {
+			char_count := char_count(data)
+			fmt.Println(char_count)
+		} else {
+			fmt.Println(word_count(data), len(content), line_count(data), char_count(data), filename)
+
+		}
+
+
+
+		
+	}
